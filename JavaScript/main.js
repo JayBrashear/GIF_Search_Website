f6283e0d480c45fa5aa18e08267425438cfd45df
JayBrashear <jeremy.brashear@hotmail.com>
2023-08-10 15:31:03 -0500

    added empty function to JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,19 @@
 $(document)
     .ready(()=>{
      
-        $("#submit").click(function(){            
+        $("#submit").click(function(){   
+            $("#GIFresults").empty();
             let userInput = $("#search").val()
             $.ajax({
                 type: "get",
-                url:`https://api.giphy.com/v1/channels/search?api_key=sb4Bp20TyZX1Hj9OCmy6BPOpR8f5429Y&q=${userInput}&limit=25&offset=0`,
+                url:`https://api.giphy.com/v1/gifs/search?api_key=sb4Bp20TyZX1Hj9OCmy6BPOpR8f5429Y&q=${userInput}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
                 data: "data",
                 dataType: "JSON",
                 success: function (response) {  
                  let GIFsearch = response.data
                  $.each(GIFsearch, (i,e)=>{
-                        let GIFuserAvatar = e.user.avatar_url
-                        $("body").append(`<img src= ${GIFuserAvatar} alt = "User Avatar"/>`)
+                        let GIFobj = e.images.original.url
+                        $("#GIFresults").append(`<img src= ${GIFobj} alt = "GIF image"/>`)                       
                  })
                 }
             });
